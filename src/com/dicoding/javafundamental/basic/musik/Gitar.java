87461792a4faffd1b09b99d48acc35b2b9a5875a
budistwn15 <budi.setiawan112001@gmail.com>
2022-02-10 18:12:25 +0700

    Membuat class gitar di package

---
+++ src/com/dicoding/javafundamental/basic/musik/Gitar.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.dicoding.javafundamental.basic.musik;
+
+/**
+ *
+ * @author Budi Setiawan
+ */
+public class Gitar {
+    public static void main(String[] args) {
+        System.out.println("Jreng...");
+    }
+}
