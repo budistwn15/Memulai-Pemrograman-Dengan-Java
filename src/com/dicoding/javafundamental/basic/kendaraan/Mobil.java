19262864a0eb38e6a25ba635e2d7995aea2d3cac
budistwn15 <budi.setiawan112001@gmail.com>
2022-02-10 18:15:25 +0700

    Menambahkan class Kereta, Mobil, dan Motor

---
+++ src/com/dicoding/javafundamental/basic/kendaraan/Mobil.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.dicoding.javafundamental.basic.kendaraan;
+
+/**
+ *
+ * @author Budi Setiawan
+ */
+public class Mobil {
+    public static void jumlahBan(){
+        System.out.println("Ban mobil ada 4");
+    }
+}
