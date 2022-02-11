fc8195601e2ec2692b056d7c4100fab52013e9a7
budistwn15 <budi.setiawan112001@gmail.com>
2022-02-11 17:34:49 +0700

    Belajar error indexoutofbond

---
+++ src/Java_Dasar/IndexOutOfBond.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Java_Dasar;
+
+/**
+ *
+ * @author Budi Setiawan
+ */
+public class IndexOutOfBond {
+    public static void main(String[] args) {
+        int[] arrA = {1, 2, 3, 4};
+        System.out.println(arrA[0]);
+        System.out.println(arrA[1]);
+        System.out.println(arrA[2]);
+        // Akses indeks ke 4
+        System.out.println(arrA[4]);
+    }
+}
