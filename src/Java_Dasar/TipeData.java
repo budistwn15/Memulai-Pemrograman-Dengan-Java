b0a21aefbc4bb57eb745ad329216e928720c5827
budistwn15 <budi.setiawan112001@gmail.com>
2022-02-11 16:11:32 +0700

    Belajar Tipe Data

---
+++ src/Java_Dasar/TipeData.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Java_Dasar;
+
+/**
+ *
+ * @author Budi Setiawan
+ */
+public class TipeData {
+    public static void main(String[] args) {
+        //1. Byte
+        byte value = 10;
+        byte anotherValue = -10;
+        
+        System.out.println("1. Byte");
+        System.out.println(value);
+        System.out.println(anotherValue);
+        
+        // 2. Short
+        short valueShort = 15000;
+        short anotherValueShort = -20000;
+        
+        System.out.println("2. Short");
+        System.out.println(valueShort);
+        System.out.println(anotherValueShort);
+        
+        // 3. Int
+        int valueInt = 150000;
+        int anotherValueInt = -200000;
+        
+        System.out.println("3. Int");
+        System.out.println(valueInt);
+        System.out.println(anotherValueInt);
+        
+        // 4. Long
+        long valueLong = 150000L;
+        long anotherValueLong = -200000L;
+        
+        System.out.println("4. Long");
+        System.out.println(valueLong);
+        System.out.println(anotherValueLong);
+        
+        // 5. Float
+        float valueFloat = 3.5f;
+        
+        System.out.println("5. Float");
+        System.out.println(valueFloat);
+        
+        // 6. Double
+        double valueDouble = 5.0;
+        System.out.println("6. Double");
+        System.out.println(valueDouble);
+        
+        // 7. Boolean
+        boolean valueBool = true;
+        boolean anotherValueBool = false;
+        
+        System.out.println("7. Boolean");
+        System.out.println(valueBool);
+        System.out.println(anotherValueBool);
+        
+        // 8. Char
+        char item = 'B';
+        System.out.println("8. Char");
+        System.out.println(item);
+    }
+}
