977af19419ec676d35221051c0b149d052edd862
budistwn15 <budi.setiawan112001@gmail.com>
2022-02-11 17:35:13 +0700

    Belajar Inisiasi Array

---
+++ src/Java_Dasar/InisiasiArray.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Java_Dasar;
+
+/**
+ *
+ * @author Budi Setiawan
+ */
+public class InisiasiArray {
+    public static void main(String[] args) {
+        int[] arrInt = new int[]{1,2,3,4,5};
+        
+        System.out.println(arrInt[0]);
+        System.out.println(arrInt[1]);
+        System.out.println(arrInt[2]);
+        System.out.println(arrInt[3]);
+        System.out.println(arrInt[4]);
+    }
+}
---
+++ src/Java_Dasar/InisiasiArray3.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Java_Dasar;
+
+/**
+ *
+ * @author Budi Setiawan
+ */
+public class InisiasiArray3 {
+    public static void main(String[] args) {
+        int[] arrInt = {1,2,3,4};
+        System.out.println(arrInt[0]);
+        System.out.println(arrInt[1]);
+        System.out.println(arrInt[2]);
+        System.out.println(arrInt[3]);
+    }
+}
